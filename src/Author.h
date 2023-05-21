3939bee243941978155c607eb8ee8f9b2b9aea01
ntatrishvili <nia.tatrishvili@gmail.com>
2023-05-21 17:33:06 +0200

    added comments

--- src/Author.h
+++ src/Author.h
@@ -7,11 +7,15 @@ class Author
     std::string affiliation;
 
 public:
+    //constructors for all cases
     Author(std::string _name, std::string _affiliation);
     Author(std::string _name);
     Author();
+    //destructor
     ~Author(){}
+    //getters for all fields
     std::string getName();
     std::string getAffiliation();
+    //toString to print the class well (for testing)
     std::string toString();
 };
--- src/Comment.h
+++ src/Comment.h
@@ -9,17 +9,22 @@ class Comment
     std::string commentText;
 
 public:
+    //constructors for all cases
     Comment(int _pagesNum, int _figuresNum, int _tablesNum, std::string _commentText);
     Comment(int _pagesNum, int _figuresNum, int _tablesNum);
     Comment(int _pagesNum, int _figuresNum);
     Comment(int _pagesNum);
     Comment();
 
+    //destructor
     ~Comment(){}
     
+    //getters to access the fields
     int getPageNum();
     int getFigureNum();
     int getTableNum();
     std::string getCommentText();
+
+    //toString to output the class better
     std::string toString();
 };
